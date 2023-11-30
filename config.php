8fe7b5bd7050b4175a6a37252b0447b7fda7903c
Mathéo Vignaud <64284499+Pokecreatorfr@users.noreply.github.com>
2023-11-30 16:57:33 +0100

    Create config.php

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+$servername = "BDD_HOSTNAME";
+$username = "BDD_USERNAME";
+$password = "BDD_PASSWORD";
+
+try {
+    $pdo = new PDO("mysql:host=$servername;dbname=cesi", $username, $password);
+    // set the PDO error mode to exception
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Erreur de connexion à la base de donnée: " . $e->getMessage();
+} 
+?>
