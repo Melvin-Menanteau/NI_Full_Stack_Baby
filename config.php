bc81ad9b7e218d76798389a06310fe9ceb325a81
Louis F <62678565+LouisFourre@users.noreply.github.com>
2023-11-30 17:19:10 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 
-$servername = "SBDD_HOSTNAME";
-$username = "SBDD_USERNAME";
-$password = "SBDD_PASSWORD";
+$servername = getenv("SBDD_HOSTNAME");
+$username = getenv("SBDD_USERNAME");
+$password = getenv("SBDD_PASSWORD"°;
 
 try {
     $pdo = new PDO("mysql:host=$servername;dbname=if0_35531073_test", $username, $password);
