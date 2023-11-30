1638098fd71ffb0088bc529a6e0d20fa0f65ac13
Louis F <62678565+LouisFourre@users.noreply.github.com>
2023-11-30 17:03:43 +0100

    Update config.php

--- config.php
+++ config.php
@@ -5,7 +5,7 @@ $username = "BDD_USERNAME";
 $password = "BDD_PASSWORD";
 
 try {
-    $pdo = new PDO("mysql:host=$servername;dbname=cesi", $username, $password);
+    $pdo = new PDO("mysql:host=$servername;dbname=f0_35531073_test", $username, $password);
     // set the PDO error mode to exception
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
