786b41b8bf7052c43e70c053a841e24e38e10485
Melvin MENANTEAU <melvin.menanteau@soriba.fr>
2023-12-05 12:14:19 +0100

    feat (dev/index): API

--- pages/dev/index.js
+++ pages/dev/index.js
@@ -2,11 +2,20 @@ import { useState } from "react";
 
 export default function DevIndex() {
     const [text, setText] = useState('test');
+    const [time, setTime] = useState(0);
+
+    const fetchTime = async () => {
+        const res = await fetch('/api/time');
+        const json = await res.json();
+        setTime(json.time);
+    };
 
     return (
         <>
             <input type="text" onChange={(e) => setText(e.target.value)} />
             <div>{ text }</div>
+            <button onClick={() => fetchTime()}>API</button>
+            <div>{ time }</div>
         </>
     );
 }
